affd2bda04442d17ad587f01629e8509089c79eb
Austin Childress <bio4554@Austins-iMac.local>
2017-02-13 21:26:09 -0600

    First

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginUserField: UITextField!
+    @IBOutlet weak var loginPassField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginPressed(_ sender: UIButton) {
+        //FIRAuth.auth()?.createUser(withEmail: <#T##String#>, password: <#T##String#>, completion: <#T##FIRAuthResultCallback?##FIRAuthResultCallback?##(FIRUser?, Error?) -> Void#>)
+    }
 
 }
 
